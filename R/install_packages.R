0a0c45cb629e29bf360c577b745ae8a5f47cecc0
Kyle M. Lang <kylelang86@gmail.com>
2025-07-22 01:33:45 +0200

    basic template structure is done; adding some minimal instructions to the README

--- R/install_packages.R
+++ R/install_packages.R
@@ -1,4 +1,9 @@
-renv::status()
-renv::install("remotes")
-renv::update()
-renv::snapshot()
+### Title:    Package Management via renv
+### Author:   Kyle M. Lang
+### Modified: 2025-07-21
+
+renv::restore()
+
+# renv::status()
+# renv::update()
+# renv::snapshot()
